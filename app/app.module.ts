a48a6b86820381e6fc5ff3bdac85b998f194f569
Jörg Holger <s0550352@htw-berlin.de>
2017-01-28 17:06:08 +0100

    improved info icon

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,15 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+import {HttpModule, JsonpModule} from "@angular/http";
 
 import {AppComponent}  from './app.component';
 import {InputComponent} from "./components/input.component";
 import {OutputComponent} from "./components/output.component";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
 import {SafeHtmlPipe} from "./pipes/safe-html.pipe";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpModule],
+  imports: [BrowserModule, FormsModule, HttpModule, JsonpModule],
   declarations: [AppComponent, InputComponent, OutputComponent, SafeHtmlPipe],
   bootstrap: [AppComponent],
   entryComponents: [OutputComponent]
--- app/components/input.component.ts
+++ app/components/input.component.ts
@@ -1,14 +1,21 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {WikiPlagAPIService} from '../services/wikiplag-api.service';
 
 @Component({
   selector: 'input-comp',
   templateUrl: './app/components/input.component.html',
+  providers: [WikiPlagAPIService],
 })
 export class InputComponent  {
   inputText:string;
   @Output() sendEventEmitter = new EventEmitter();
 
+  constructor(private wikiPlagApiService: WikiPlagAPIService) {}
+
   send() {
+    console.log("Hier ist send()");
+    this.wikiPlagApiService.postInputText(this.inputText).subscribe(res => {
+      console.log(res);});
     this.sendEventEmitter.emit();
     this.inputText = '';
   }
