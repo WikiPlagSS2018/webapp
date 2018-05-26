a1566864a6cf65b5f0c2a52ef2a01d9d7ece0d6a
marvpaul <marvin.krueger@yahoo.de>
2018-05-26 19:40:57 +0200

    Added jspdf and a small patch because its not yet official compatible with ng 6

--- src/app/services/pdf-generator.service.ts
+++ src/app/services/pdf-generator.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { PlagResponse } from '../models/responses/plag-response';
+import * as jsPDF from 'jspdf'
+
 
 @Injectable()
 export class PdfGeneratorService {
@@ -8,5 +10,16 @@ export class PdfGeneratorService {
 
   generatePDF(plagResponse: PlagResponse){
     console.log("Generating a pdf is coming soon ..." + plagResponse);
+    // Default export is a4 paper, portrait, using milimeters for units
+    var doc = new jsPDF();
+
+    doc.setFontSize(22);
+    doc.text(plagResponse.name);
+
+    doc.setFontSize(18);
+
+    var splitTitle = doc.splitTextToSize(plagResponse.tagged_input_text, 180);
+    doc.text(15, 20, splitTitle);
+    doc.save('a4.pdf');
   }
 }
